acf235c087865679148ccf7d69a5d88211a3f132
Oumaima-Younsi1 <oumaima.younsi@isgb.ucar.tn>
2026-05-01 22:11:12 +0200

    Initial commit

---
+++ Oumi/src/main/java/isgb/java/Oumi/Service/InstructorService.java
@@ -0,0 +1,38 @@
+package isgb.java.Oumi.Service;
+
+import isgb.java.Oumi.Model.Instructor;
+import isgb.java.Oumi.Repository.InstructorRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class InstructorService {
+
+    private InstructorRepository instructorRepository;
+
+    public Instructor createInstructor(Instructor instructor) {
+        if (instructor.getCourses() != null) {
+            instructor.getCourses().forEach(course -> {
+                if (course.getInstructors() != null &&
+                        !course.getInstructors().contains(instructor)) {
+                    course.getInstructors().add(instructor);
+                }
+            });
+        }
+        return instructorRepository.save(instructor);
+    }
+
+    public List<Instructor> findByName(String name) {
+        return instructorRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    public Instructor findById(int id) {
+        return instructorRepository.findByIdWithCourses(id)
+                .orElseThrow(() -> new RuntimeException("Instructor non trouvé"));
+    }
+}
